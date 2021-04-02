57d2f802c992d213721e04581b6193a4106c998c
Prokopiv <halyna.kotlyar@gmail.com>
2021-04-02 12:15:28 +0300

    package

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
-import TodosPage from './pages/TodosPage';
+// import TodosPage from './pages/TodosPage';
+import Container from './components/Container';
 
 function App() {
- 
-  return (
-    <div className="App">
-      <TodosPage />
-    </div>
-  );
+  return;
+  <Container>
+    // <div className="App">{/* <TodosPage /> */}</div>;
+  </Container>;
 }
 
 export default App;
---
+++ src/components/Container/Container.jsx
@@ -0,0 +1,7 @@
+import React, { memo } from 'react';
+
+const Container = ({ children }) => {
+  return <div>{children}</div>;
+};
+
+export default Container;
