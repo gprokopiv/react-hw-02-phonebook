04e81ea5def5918f7253883d46bb30e9b36b94ad
Prokopiv <halyna.kotlyar@gmail.com>
2021-04-05 23:10:11 +0300

    filter

--- src/App.js
+++ src/App.js
@@ -4,9 +4,15 @@ import { v4 as uuidv4 } from 'uuid';
 
 class App extends Component {
   state = {
-    contacts: [],
+    contacts: [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
     name: '',
     number: '',
+    filter: '',
   };
   handleSubmit = e => {
     e.preventDefault();
@@ -18,10 +24,20 @@ class App extends Component {
 
     this.setState({ [name]: value });
   };
+  addContact = ({ name, number }) => {
+    const id = uuidv4();
+    const contact = {
+      name,
+      number,
+    };
+  };
+  onChange = e => {
+    this.setState({ filter: e.currentTarget.value });
+  };
   render() {
     const { name, number } = this.state;
     return (
-      <>
+      <Container>
         <h1>Phonebook</h1>
         <form onSubmit={this.handleSubmit}>
           <label>Name</label>
@@ -35,25 +51,30 @@ class App extends Component {
             title="Имя может состоять только из букв, апострофа, тире и пробелов. Например Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan и т. п."
             required
           />
-        </form>
 
-        <br></br>
-        <p>Number</p>
-        <input
-          type="tel"
-          name="number"
-          pattern="(\+?( |-|\.)?\d{1,2}( |-|\.)?)?(\(?\d{3}\)?|\d{3})( |-|\.)?(\d{3}( |-|\.)?\d{4})"
-          title="Номер телефона должен состоять из 11-12 цифр и может содержать цифры, пробелы, тире, пузатые скобки и может начинаться с +"
-          required
-        />
-        <button>Add contact</button>
+          <br></br>
+          <label>Number</label>
+          <input
+            type="tel"
+            name="number"
+            value={number}
+            onChange={this.handleChange}
+            placeholder="111-111-111"
+            pattern="(\+?( |-|\.)?\d{1,2}( |-|\.)?)?(\(?\d{3}\)?|\d{3})( |-|\.)?(\d{3}( |-|\.)?\d{4})"
+            title="Номер телефона должен состоять из 11-12 цифр и может содержать цифры, пробелы, тире, пузатые скобки и может начинаться с +"
+            required
+          />
+          <button onSubmit={this.addContact}>Add contact</button>
+        </form>
         <h2>Contacts</h2>
+        <label>Find cntacts by name </label>
+        <input type="text" value={this.filter} onChange={this.onChange} />
         <ul>
           <li>Rosie SImpson</li>
           <li>Eden Clements</li>
           <li>Hermione Kline</li>
         </ul>
-      </>
+      </Container>
     );
   }
 }
