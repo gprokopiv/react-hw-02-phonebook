b237fa85e25fc65b4a9029fe36a2b36f93ba3144
Prokopiv <halyna.kotlyar@gmail.com>
2021-04-06 11:17:28 +0300

    contactForm

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Container from './components/Container/';
 import { v4 as uuidv4 } from 'uuid';
+import ContactForm from './components/ContactForm';
 
 class App extends Component {
   state = {
@@ -14,16 +15,16 @@ class App extends Component {
     number: '',
     filter: '',
   };
-  handleSubmit = e => {
-    e.preventDefault();
-    this.props.onSubmit(this.state);
-    this.setState({ name: '', number: '' });
-  };
-  handleChange = e => {
-    const { name, value } = e.currentTarget;
+  // handleSubmit = e => {
+  //   e.preventDefault();
+  //   this.props.onSubmit(this.state);
+  //   this.setState({ name: '', number: '' });
+  // };
+  // handleChange = e => {
+  //   const { name, value } = e.currentTarget;
 
-    this.setState({ [name]: value });
-  };
+  //   this.setState({ [name]: value });
+  // };
 
   addContact = ({ name, number }) => {
     const id = uuidv4();
@@ -48,33 +49,8 @@ class App extends Component {
     return (
       <Container>
         <h1>Phonebook</h1>
-        <form onSubmit={this.handleSubmit}>
-          <label>Name</label>
-          <input
-            type="text"
-            name="name"
-            value={name}
-            placeholder="John Smith"
-            onChange={this.handleChange}
-            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-            title="Имя может состоять только из букв, апострофа, тире и пробелов. Например Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan и т. п."
-            required
-          />
+        <ContactForm onSubmit={this.addContact} />
 
-          <br></br>
-          <label>Number</label>
-          <input
-            type="tel"
-            name="number"
-            value={number}
-            onChange={this.handleChange}
-            placeholder="111-111-111"
-            pattern="(\+?( |-|\.)?\d{1,2}( |-|\.)?)?(\(?\d{3}\)?|\d{3})( |-|\.)?(\d{3}( |-|\.)?\d{4})"
-            title="Номер телефона должен состоять из 11-12 цифр и может содержать цифры, пробелы, тире, пузатые скобки и может начинаться с +"
-            required
-          />
-          <button onSubmit={this.addContact}>Add contact</button>
-        </form>
         <h2>Contacts</h2>
         <label>Find cntacts by name </label>
         <input type="text" value={this.filter} onChange={this.onChange} />
---
+++ src/components/ContactForm/ContactForm.jsx
@@ -0,0 +1,54 @@
+import React, { Component } from 'react';
+
+class ContactForm extends Component {
+  state = {
+    name: '',
+    number: '',
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+    this.props.onSubmit(this.state);
+    this.setState({ name: '', number: '' });
+  };
+  handleChange = e => {
+    const { name, value } = e.currentTarget;
+
+    this.setState({ [name]: value });
+  };
+
+  render() {
+    const { name, number } = this.state;
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label>Name</label>
+        <input
+          type="text"
+          name="name"
+          value={name}
+          placeholder="John Smith"
+          onChange={this.handleChange}
+          pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+          title="Имя может состоять только из букв, апострофа, тире и пробелов. Например Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan и т. п."
+          required
+        />
+
+        <br></br>
+        <label>Number</label>
+        <input
+          type="tel"
+          name="number"
+          value={number}
+          onChange={this.handleChange}
+          placeholder="111-111-111"
+          pattern="(\+?( |-|\.)?\d{1,2}( |-|\.)?)?(\(?\d{3}\)?|\d{3})( |-|\.)?(\d{3}( |-|\.)?\d{4})"
+          title="Номер телефона должен состоять из 11-12 цифр и может содержать цифры, пробелы, тире, пузатые скобки и может начинаться с +"
+          required
+        />
+        <button onSubmit={this.addContact}>Add contact</button>
+      </form>
+    );
+  }
+}
+
+export default ContactForm;
