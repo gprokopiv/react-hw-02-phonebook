707b1a1ed4a82156cc916546fd45fcfd10a01dec
Prokopiv <halyna.kotlyar@gmail.com>
2021-05-24 13:19:23 +0300

    step 1

--- src/App.js
+++ src/App.js
@@ -1,22 +1,27 @@
 import React, { Component } from 'react';
 import Container from './components/Container/';
-import { v4 as uuidv4 } from 'uuid';
 import ContactForm from './components/ContactForm';
 import Filter from './components/Filter';
 import ContactList from './components/ContactList/ContactList/';
+import { v4 as uuidv4 } from 'uuid';
 
 class App extends Component {
   state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
+    contacts: [],
     name: '',
-    number: '',
-    filter: '',
   };
+
+  // state = {
+  //   contacts: [
+  //     { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+  //     { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+  //     { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+  //     { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+  //   ],
+  //   name: '',
+  //   number: '',
+  //   filter: '',
+  // };
   // handleSubmit = e => {
   //   e.preventDefault();
   //   this.props.onSubmit(this.state);
@@ -28,53 +33,76 @@ class App extends Component {
   //   this.setState({ [name]: value });
   // };
 
-  addContact = ({ name, number }) => {
-    const contact = {
-      id: uuidv4(),
-      name,
-      number,
-    };
+  // addContact = ({ name, number }) => {
+  //   const contact = {
+  //     id: uuidv4(),
+  //     name,
+  //     number,
+  //   };
 
-    const { contacts } = this.state;
+  //   const { contacts } = this.state;
 
-    if (
-      contacts.find(
-        contact => contact.name.toLowerCase() === name.toLowerCase(),
-      )
-    ) {
-      alert(`${name} is already in contacts.`);
-    } else if (contacts.find(contact => contact.number === number)) {
-      alert(`${number} is already in contacts.`);
-    } else if (name.trim() === '' || number.trim() === '') {
-      alert("Enter the contact's name and number phone!");
-    } else if (!/\d{3}[-]\d{2}[-]\d{2}/g.test(number)) {
-      alert('Enter the correct number phone!');
-    } else {
-      this.setState(({ contacts }) => ({
-        contacts: [contact, ...contacts],
-      }));
-    }
-  };
-  onChange = e => {
-    this.setState({ filter: e.currentTarget.value });
-  };
-  deleteContact = contactId => {
-    this.setState(prevState => ({
-      contacts: prevState.filter(contact => contact.id !== contactId),
-    }));
-  };
+  //   if (
+  //     contacts.find(
+  //       contact => contact.name.toLowerCase() === name.toLowerCase(),
+  //     )
+  //   ) {
+  //     alert(`${name} is already in contacts.`);
+  //   } else if (contacts.find(contact => contact.number === number)) {
+  //     alert(`${number} is already in contacts.`);
+  //   } else if (name.trim() === '' || number.trim() === '') {
+  //     alert("Enter the contact's name and number phone!");
+  //   } else if (!/\d{3}[-]\d{2}[-]\d{2}/g.test(number)) {
+  //     alert('Enter the correct number phone!');
+  //   } else {
+  //     this.setState(({ contacts }) => ({
+  //       contacts: [contact, ...contacts],
+  //     }));
+  //   }
+  // };
+  // onChange = e => {
+  //   this.setState({ filter: e.currentTarget.value });
+  // };
+  // deleteContact = contactId => {
+  //   this.setState(prevState => ({
+  //     contacts: prevState.filter(contact => contact.id !== contactId),
+  //   }));
+  // };
 
-  visibleContacts = () => {
-    return this.contact.name.toLowerCase().inclused(this.filter.toLowerCase());
-  };
+  // visibleContacts = () => {
+  //   return this.contact.name.toLowerCase().inclused(this.filter.toLowerCase());
+  // };
   render() {
-    const { name, number, filter, contacts } = this.state;
+    // const { name, number, filter, contacts } = this.state;
     return (
       <Container>
         <h1>Phonebook</h1>
-        <ContactForm onSubmit={this.addContact} />
+        <form>
+          <label>
+            Name
+            <input
+              name="name"
+              pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+              title="Имя может состоять только из букв, апострофа, тире и пробелов. Например Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan и т. п."
+              required
+              // value={login}
+              // onChange={this.handleChange}
+            />
+          </label>
 
-        <h2>Contacts</h2>
+          <button type="submit">Add contact</button>
+        </form>
+        <h2> Contacts</h2>
+        <ul>
+          <li>Rosie Simpson</li>
+          <li>Hermione Kline</li>
+          <li>Eden Clements</li>
+        </ul>
+
+        {/* <h1>Phonebook</h1>
+        <ContactForm onSubmit={this.addContact} /> */}
+
+        {/* <h2>Contacts</h2> */}
         {/* {contacts.length > 1 && (
           <Filter value={this.filter} onChange={this.changeFilter} />
         )}
